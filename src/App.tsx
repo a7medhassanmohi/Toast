065c4120c156511187dfd8c9c38e87060354f2d1
Ahmed Hassan Mohi <a7med.hassan.mohi@gmail.com>
2023-11-10 15:57:06 +0200

    init toast app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import styles from "./App.module.scss"
+function App() {
+
+  return (
+    <>
+    <div className="text-blue-600">
+      mohi
+
+    </div>
+    </>
+  )
+}
+
+export default App
